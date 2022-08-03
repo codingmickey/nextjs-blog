8536104689c4775e2fae8b018d8e3c562ed7e472
Kartik <kjmickey003@gmail.com>
2022-08-04 04:10:54 +0530

    Pre-rendering and Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,18 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -10,13 +21,39 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          Hi this is Kartik Jolapara aka codingmickey. I hate to build websites
-          then too I don't know why am I learning <kbd>nextjs</kbd>
+          Hi this is Kartik Jolapara aka <kbd>codingmickey</kbd>. I hate to
+          build websites, but here you are in one of them 😈
         </p>
         <p>
+          You can conctact me at either at{' '}
+          <a href='https://twitter.com/codingmickey' className='href'>
+            Twitter🐦
+          </a>
+          or by{' '}
+          <a href="mailto:kjmickey003@gmail.com?subject=Hey%20Kartik%2C%20Let's%20connect!&body=%3CAny%20message%3E">
+            mail📧
+          </a>
+        </p>
+        {/* <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        </p> */}
+      </section>
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
---
+++ posts/jp-morgan-cfg.md
@@ -0,0 +1,13 @@
+---
+title: 'Code for Good 2022 by JP Morgan'
+date: '04-08-2022'
+---
+
+(SPOILER) This was one of the most amazing moments of my life. Excitment. Joy. Frustration. Nervousness.
+
+So starting from the selection process I
+
+- **Static Generation** is the pre-rendering method that generates the HTML at **build time**. The pre-rendered HTML is then _reused_ on each request.
+- **Server-side Rendering** is the pre-rendering method that generates the HTML on **each request**.
+
+Importantly, Next.js lets you **choose** which pre-rendering form to use for each page. You can create a "hybrid" Next.js app by using Static Generation for most pages and using Server-side Rendering for others.
